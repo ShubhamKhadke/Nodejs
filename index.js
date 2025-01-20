23009e9ed672da0ce05e867826b888089d7913f0
shubhamkhadke <skhadke811@gmail.com>
2025-01-20 16:40:08 +0530

    Done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('100% work krnar ');
+ res.send('here comes result ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
