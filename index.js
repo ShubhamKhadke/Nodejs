08f919427d8633a9c1f6783dd5f4692ff0d97c89
shubhamkhadke <skhadke811@gmail.com>
2025-01-21 10:56:05 +0530

    Ho jana chahiye

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 xyvar express = require('express'); 
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('here comes result ');
+ res.send('Yes did it! ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
